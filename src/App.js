400582be4a70f79198bb841bcbb48cc6ef6e7210
JaniceChua <janiceisdoraemon@gmail.com>
2024-08-16 10:36:01 +0800

    update package

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
                 <Routes>
                     <Route
                         exact
-                        path="/"
+                        path="/Home"
                         element={<Home />}
                     />
                     <Route
